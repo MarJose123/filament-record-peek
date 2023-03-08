59bc348b889268324628dbd616b308b6e5fe53c3
Marjose <hypernate1@gmail.com>
2023-03-08 13:07:00 +0800

    WIP

---
+++ src/Tables/Actions/PeekAction.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace MarJose123\FilamentRecordPeek\Tables\Actions;
+
+
+use Closure;
+use Filament\Forms\ComponentContainer;
+use Filament\Tables\Actions\Action;
+use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Actions\ViewAction;
+use Illuminate\Database\Eloquent\Model;
+use Filament\Forms;
+
+
+class PeekAction extends Action
+{
+    protected ?Closure $mutateRecordDataUsing = null;
+
+    public static function getDefaultName(): ?string
+    {
+        return 'peek';
+    }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->label('Peek');
+
+        $this->color('secondary');
+
+        $this->icon('heroicon-s-arrows-expand');
+
+        $this->groupedIcon('heroicon-s-arrows-expand');
+
+
+        $this->modalHeading(fn (): string => __('filament-support::actions/view.single.modal.heading', ['label' => $this->getRecordTitle()]));
+
+        $this->modalActions(fn (): array => array_merge(
+            $this->getExtraModalActions(),
+            [
+                $this->getModalCancelAction()->label(__('filament-support::actions/view.single.modal.actions.close.label')),
+            ],
+        ));
+
+        $this->disableForm();
+
+        $this->mountUsing(function (ComponentContainer $form, Model $record): void {
+            $data = $record->attributesToArray();
+
+            if ($this->mutateRecordDataUsing) {
+                $data = $this->evaluate($this->mutateRecordDataUsing, ['data' => $data]);
+            }
+
+            $form->fill($data);
+        });
+
+        $this->form($this->getFormSchema());
+
+
+        $this->action(static function (): void {
+        });
+
+        $this->slideOver(true);
+
+    }
+    public function mutateRecordDataUsing(?Closure $callback): static
+    {
+        $this->mutateRecordDataUsing = $callback;
+
+        return $this;
+    }
+
+}
